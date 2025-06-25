2aab94257f9179b9a0294f5ced18e7133ee76885
Elin Rowlands <elinrow@gmail.com>
2025-06-25 16:14:14 +0100

    Update

--- Analysis/Pooled Results.R
+++ Analysis/Pooled Results.R
@@ -2,6 +2,8 @@ pooled_models(data, threshold = 100)
 
 pooled_plot <- model_plot(pooled_results)
 
-ggsave("Results/pooled_plot.png", plot = pooled_plot)
+ggsave("Results/pooled_plot.png", plot = pooled_plot, bg = "white",
+       width = 10)
 
 write.csv(pooled_results, "Results/pooledResults.csv")
+
